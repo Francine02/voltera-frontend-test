fd59df139eea540e7ecd984b329575a81a2fb85e
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-07-03 19:34:11 -0300

    feat(components): componente Title com texto estilizado e responsivo

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,32 @@
+<div>
+	<h2>Hey!</h2>
+	<h1 id="title">Descubra a <span>idade</span> que o <span>seu nome revela!</span></h1>
+
+	<h3>Digite seu nome e surpreenda-se com a estimativa de idade</h3>
+</div>
+
+<style>
+	h2 {
+		font-size: 2rem;
+	}
+
+	h1 {
+		span {
+			background: linear-gradient(90deg, black, #c9b741);
+			-webkit-background-clip: text;
+			-webkit-text-fill-color: transparent;
+			background-clip: text;
+			color: transparent;
+		}
+
+		margin: 0 0 1rem;
+		font-size: 3rem;
+	}
+
+	@media (max-width: 768px) {
+		h1 {
+			margin: 0 0 1rem;
+			font-size: 2rem;
+		}
+	}
+</style>
