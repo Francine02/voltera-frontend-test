19f488b7ce20e4ef882c1efcbb983901035b9c59
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-07-03 19:33:22 -0300

    feat(components): componente Result para exibir idade estimada

---
+++ src/lib/components/Result.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let data;
+</script>
+
+{#if data.age}
+	<p>{data.name} tem cerca de <strong>{data.age}</strong> anos.</p>
+{/if}
+
+<style>
+	p {
+		margin-top: 2rem;
+		font-size: 1rem;
+		text-align: center;
+		color: #161616;
+	}
+
+	strong {
+		color: #998b2c;
+	}
+
+	@media (max-width: 768px) {
+		p {
+			font-size: 0.9rem;
+		}
+	}
+</style>
