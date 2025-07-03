20f3fd1da90de0d038f2e2b812f124ebcf8dfaed
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-07-03 19:40:20 -0300

    feat(routes): componentes Title, Input e Result em arquivo principal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,45 @@
+<script>
+	import Input from '$lib/components/Input.svelte';
+	import Result from '$lib/components/Result.svelte';
+	import Title from '$lib/components/Title.svelte';
 
+	export let data;
+</script>
+
+<main>
+	<div>
+		<Title />
+
+		<Input {data} />
+		<Result {data} />
+	</div>
+</main>
+
+<style>
+	:global(*) {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		vertical-align: baseline;
+		font-family: monospace;
+	}
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background-color: #f3f2f2;
+		height: 100vh;
+		text-align: start;
+	}
+
+	div {
+		max-width: 37rem;
+	}
+
+	@media (max-width: 768px) {
+		div {
+			max-width: 25rem;
+		}
+	}
+</style>
